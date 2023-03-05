87647a7f4f89754606aefc6c8d331cf595db15e6
ly-temp <100673133+ly-temp@users.noreply.github.com>
2023-03-05 10:15:13 +0000

    update

---
+++ record/list_1/summary.md
@@ -0,0 +1,3 @@
+![3033.HK](https://github.com/ly-temp/github_action_playground/workspaces/github_action_playground/record/list_1/image/3033.HK.png)
+![1234.HK](https://github.com/ly-temp/github_action_playground/workspaces/github_action_playground/record/list_1/image/1234.HK.png)
+![3032.HK](https://github.com/ly-temp/github_action_playground/workspaces/github_action_playground/record/list_1/image/3032.HK.png)
--- update.py
+++ update.py
@@ -7,38 +7,57 @@ import os
 
 record_dir = Path("./record").resolve()
 list_dir = Path("./stock_list").resolve()
+img_dir = Path("image")
+summary_dir = Path("summary.md")
 
 def plot_graph(name, hist):
+    img_name = name+'.png'
+
     plt.plot(hist['High'], marker="o", color="red", label="High")
     plt.plot(hist['Low'], marker="o", color="green", label="Low")
     plt.plot(hist['Average'], marker="o", color="brown", label="Average")
     plt.xticks(rotation=50, fontsize = 'small')
-    plt.ylabel('price')
-    plt.xlabel('date')
-    plt.legend()
+    #plt.ylabel('price')
+    #plt.xlabel('date')
     plt.grid()
-    plt.savefig(name+'.png', bbox_inches='tight')
+    plt.legend()
+    plt.savefig(img_name, bbox_inches='tight')
+    plt.clf()
+    return img_name
 
 def update_stock(name):
     ticket = yf.Ticker(name)
     hist = ticket.history(period="7d")
     hist['Average'] = hist[['High','Low']].mean(axis=1)
-    plot_graph(name, hist)
+    img_name = plot_graph(name, hist)
+    return Path(img_name).resolve()
 
+mkdir_p = lambda path: path.mkdir(parents=True, exist_ok=True)
 #init
-record_dir.mkdir(parents=True, exist_ok=True)
+mkdir_p(record_dir)
 
 for f in os.listdir(list_dir):
     print(f)
     f_stem = Path(f).stem
+
     f_set_dir = record_dir.joinpath(f_stem)
-    f_set_dir.mkdir(parents=True, exist_ok=True)
-    with open(list_dir.joinpath(f)) as file:
-        for line in file:
-            os.chdir(f_set_dir)
-            stock_name = line.strip()
-            update_stock(stock_name)
-            os.chdir(record_dir)
+    mkdir_p(f_set_dir)
+
+    with open(f_set_dir.joinpath(summary_dir), "w+") as summary_f:
+
+        img_dir = f_set_dir.joinpath(img_dir)
+        mkdir_p(img_dir)
+
+        with open(list_dir.joinpath(f)) as file:
+            for line in file:
+                os.chdir(f_set_dir.joinpath(img_dir))
+                stock_name = line.strip()
+                output_img_dir = update_stock(stock_name)
+                print(stock_name)
+                os.chdir(record_dir)
+                img_url = "https://github.com/"+os.environ['GITHUB_REPOSITORY']+str(output_img_dir)
+                summary_f.write("![{}]({})\n".format(stock_name, img_url))
+    
 
 exit(1)
 

6a1bdef0d84323cb90920d8c245d0d9a1430b316
ly-temp <100673133+ly-temp@users.noreply.github.com>
2023-03-05 08:04:12 +0000

    update

--- update.py
+++ update.py
@@ -1,8 +1,12 @@
 import yfinance as yf
 import pandas as pd
 from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
 
 ticket = yf.Ticker("MSFT")
 hist = ticket.history(period="5d")
 ticket.get_shares_full(start=datetime.today()-timedelta(days=5), end=None)
 print(hist)
+plt.plot(hist, color="black")
+plt.xticks(rotation=50, fontsize = 'small')
+plt.savefig('out.png', bbox_inches='tight')
